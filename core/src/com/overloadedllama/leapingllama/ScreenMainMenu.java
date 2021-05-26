aee1ad1e61082525425539a5d05f2d47f20beabb
Giacomo Salici <giambo2000@gmail.com>
2021-05-26 22:06:17 +0200

    init stage and actors

--- core/src/com/overloadedllama/leapingllama/ScreenMainMenu.java
+++ core/src/com/overloadedllama/leapingllama/ScreenMainMenu.java
@@ -2,9 +2,14 @@ package com.overloadedllama.leapingllama;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -14,16 +19,27 @@ public class ScreenMainMenu extends Stage implements Screen {
     final GameApp game;
     OrthographicCamera camera;
     ExtendViewport viewport;
+    Stage stage;
+    Actor start;
 
 
     public ScreenMainMenu(final GameApp game) {
         this.game = game;
+
+        start = new Actor();
+        start.setColor(Color.WHITE);
+        start.setBounds(50,50,50,50);
+        stage = new Stage();
+        stage.addActor(start);
+
+        stage.addActor(new Actor());
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(GameApp.WIDTH, GameApp.HEIGHT, camera);
         viewport.apply();
         camera.position.set(GameApp.WIDTH / 2, GameApp.HEIGHT  / 2, 0);
         camera.update();
+
     }
 
     @Override
@@ -33,6 +49,8 @@ public class ScreenMainMenu extends Stage implements Screen {
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
+
+
         game.batch.begin();
         game.font.setColor(0 , 255, 0, 1);
         game.font.draw(game.batch, "Tap anywhere to begin!", 2, 50);
@@ -40,6 +58,8 @@ public class ScreenMainMenu extends Stage implements Screen {
         game.batch.draw(logo,GameApp.WIDTH/2 - (float) logo.getWidth()/4, GameApp.HEIGHT/2 - (float) logo.getHeight()/4, (float) logo.getWidth()/2, (float) logo.getHeight()/2);
         //the divisions for 4 in the x and y above are due to the resize of the w and h
         game.batch.end();
+
+        stage.draw();
 
         if (Gdx.input.isTouched()) {
            // game.setScreen(new Space(game));
