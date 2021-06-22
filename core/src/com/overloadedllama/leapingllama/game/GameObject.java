d22c34f945cf64069b5ab7f90efbbee626e9e05c
Giacomo Salici <giambo2000@gmail.com>
2021-06-23 01:07:57 +0200

    new stuff for gameplay

---
+++ core/src/com/overloadedllama/leapingllama/game/GameObject.java
@@ -0,0 +1,35 @@
+package com.overloadedllama.leapingllama.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+
+public class GameObject extends Actor {
+
+        Sprite sprite;
+
+        public GameObject(Texture texture) {
+            sprite = new Sprite(texture);
+        }
+
+        @Override
+        public void act(float delta) {
+            super.act(delta);
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            sprite.draw(batch);
+        }
+
+        public void setBounds(int x, int y, int w, int h){
+            sprite.setBounds(x, y, w, h);
+        }
+    }
+
--- core/src/com/overloadedllama/leapingllama/screens/GameScreen.java
+++ core/src/com/overloadedllama/leapingllama/screens/GameScreen.java
@@ -2,46 +2,72 @@ package com.overloadedllama.leapingllama.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.overloadedllama.leapingllama.GameApp;
+import com.overloadedllama.leapingllama.game.Llama;
 
+
+//this is the screen of the gameplay, i start to set up the environment.
 
 public class GameScreen implements Screen {
-    private Texture lamaImage;
-    private final OrthographicCamera camera;
-    private final ExtendViewport viewport;
+    private Texture llamaImage;
+  // private OrthographicCamera camera;
+    private Viewport viewport;
     GameApp game;
+
+    private Stage stage;
+    private Llama llama;
 
     public GameScreen(final GameApp game) {
         this.game = game;
 
-        camera = new OrthographicCamera();
 
-        viewport = new ExtendViewport(GameApp.WIDTH, GameApp.HEIGHT, camera);
-        viewport.apply();
-        camera.position.set(GameApp.WIDTH / 2, GameApp.HEIGHT  / 2, 0);
 
-        camera.update();
+        llamaImage = new Texture(Gdx.files.internal("llamaphoto.png"));
+
+
     }
 
     @Override
     public void show() {
-        lamaImage = new Texture(Gdx.files.internal("lamaphoto.png"));
-        lamaImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        //camera = new OrthographicCamera();
+
+        viewport = new ExtendViewport(GameApp.WIDTH, GameApp.HEIGHT);
+       //viewport.apply();
+        //camera.position.set(GameApp.WIDTH / 2, GameApp.HEIGHT  / 2, 0);
+
+        //camera.update();
+
+        stage = new Stage(viewport);
+        llama = new Llama(llamaImage);
+
+
+        llama.setColor(Color.RED);
+        System.out.println(GameApp.WIDTH/5);
+        llama.setBounds(200,50,200,400);
+
+        stage.addActor(llama);
+
+
     }
+
 
 
     @Override
     public void render(float delta) {
-
+       // super.render();
         ScreenUtils.clear(0.1f, 0, 0.2f, 1);
 
-        camera.update();
+        //camera.update();
+        stage.act(delta);
+        stage.draw();
 
-        game.batch.setProjectionMatrix(camera.combined);
+        /*game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
         game.batch.draw(lamaImage,
@@ -57,7 +83,11 @@ public class GameScreen implements Screen {
             // Is it necessary to create a new xScreen every time?
             game.setScreen(new MainMenuScreen(game));
             dispose();
-        }
+        }*/
+
+
+
+
     }
 
     @Override
@@ -71,7 +101,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void resume() {
-        dispose();
     }
 
     @Override
@@ -81,6 +110,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        lamaImage.dispose();
+     //   lamaImage.dispose();
     }
 }
