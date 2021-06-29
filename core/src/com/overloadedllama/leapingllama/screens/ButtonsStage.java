19bf98f51839804239c0ccee15d4bb9457c62811
Giacomo Salici <giambo2000@gmail.com>
2021-06-30 00:57:19 +0200

    new (rotten) buttons and table

--- core/src/com/overloadedllama/leapingllama/screens/ButtonsStage.java
+++ core/src/com/overloadedllama/leapingllama/screens/ButtonsStage.java
@@ -11,30 +11,70 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.overloadedllama.leapingllama.GameApp;
 
+import java.util.ArrayList;
+
 public class ButtonsStage {
 
-    Stage stage;
+
 
 
     ImageButton buttonJump;
-    Skin buttonLittleSkin;
-    Table buttons;
+    Skin buttonJumpSkin;
 
+    ImageButton buttonCrouch;
+    Skin buttonCrouchSkin;
+
+    ImageButton buttonFist;
+    Skin buttonFistSkin;
+
+    ImageButton buttonShot;
+    Skin buttonShotSkin;
+
+    ImageButton buttonPause;
+    Skin buttonPauseSkin;
+
+    float buttonSize;
+
+    Table buttons;
+    Stage stage;
 
 
     public ButtonsStage(Viewport viewport, float w, float h) {
 
         stage = new Stage(viewport);
 
-        buttonLittleSkin = new Skin(Gdx.files.internal("ui/jumpButton.json"), new TextureAtlas(Gdx.files.internal("ui/jumpButton.atlas")));
 
-        buttonJump = new ImageButton(buttonLittleSkin);
 
-        buttons = new Table(buttonLittleSkin);
-        //buttons = new Table();
+        buttonJumpSkin = new Skin(Gdx.files.internal("ui/jumpButton.json"), new TextureAtlas(Gdx.files.internal("ui/jumpButton.atlas")));
+        buttonJump = new ImageButton(buttonJumpSkin);
+
+        buttonCrouchSkin = new Skin(Gdx.files.internal("ui/crouchButton.json"), new TextureAtlas(Gdx.files.internal("ui/crouchButton.atlas")));
+        buttonCrouch = new ImageButton(buttonCrouchSkin);
+
+        buttonFistSkin = new Skin(Gdx.files.internal("ui/fistButton.json"), new TextureAtlas(Gdx.files.internal("ui/fistButton.atlas")));
+        buttonFist = new ImageButton(buttonFistSkin);
+
+        buttonShotSkin = new Skin(Gdx.files.internal("ui/shotButton.json"), new TextureAtlas(Gdx.files.internal("ui/shotButton.atlas")));
+        buttonShot = new ImageButton(buttonShotSkin);
+
+        buttonPauseSkin = new Skin(Gdx.files.internal("ui/pauseButton.json"), new TextureAtlas(Gdx.files.internal("ui/pauseButton.atlas")));
+        buttonPause = new ImageButton(buttonPauseSkin);
+
+
+
+        buttons = new Table();
+
         buttons.setSize(w, h);
-       // buttonJump.getLabel().setFontScale(0.1F);
-        buttons.add(buttonJump).width(0.5f).height(0.5f).align(Align.bottomLeft);//.getActor().getLabel().setFontScale(0.05f);
+        buttonSize = 0.3f;
+
+        buttons.add(buttonPause).width(buttonSize).height(buttonSize).align(Align.topLeft);
+        buttons.row();
+        buttons.add(buttonJump).width(buttonSize).height(buttonSize).align(Align.bottomLeft);
+        buttons.add(buttonShot).width(buttonSize).height(buttonSize).align(Align.bottomRight);
+        buttons.row();
+        buttons.add(buttonCrouch).width(buttonSize).height(buttonSize).align(Align.topLeft);
+        buttons.add(buttonFist).width(buttonSize).height(buttonSize).align(Align.topRight);
+        buttons.row();
 
         stage.addActor(buttons);
 
